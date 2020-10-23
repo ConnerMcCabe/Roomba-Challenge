92d1680b3e742e3dcb3ce345fce1c88d19657c41
Conner McCabe <conner.p.mccabe@gmail.com>
2020-10-23 16:52:25 -0500

    adding components

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Room from '../Components/Room/Room';
+import Results from '../Components/Results/Results';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Test</h1>
+      <Room />
+      <Results />
     </div>
   );
 }

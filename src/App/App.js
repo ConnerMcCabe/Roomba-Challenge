394c5cf35ceef6add040191fee6ff3c7fa6e7653
Conner McCabe <conner.p.mccabe@gmail.com>
2020-11-04 14:39:22 -0600

    defining room space and project structure

--- src/App/App.js
+++ src/App/App.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Room from '../Components/Room/Room';
 import Results from '../Components/Results/Results';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Room />
-      <Results />
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <Results />
+        <Room />
+      </div>
+    );
+  }
 }
+
 
 export default App;
---
+++ src/Components/Room/Grid.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import React, { Component } from 'react';
+
+class Grid extends Component {
+  render() {
+    return (
+      <div className="container">Room box</div>
+    )
+  }
+
+}
+
+export default Room;
